7ce40355a2b06cc8b329c4774e3f0da9538f1589
dimaakimm <dmitriykim2005@gmail.com>
2024-06-13 16:58:05 +0300

    merge with dev2

--- core/utils/dbConnection.py
+++ core/utils/dbConnection.py
@@ -33,8 +33,13 @@ class Request:
         await self.connector.execute(query)
 
     async def add_data_admin(self, data):
-        query = f"INSERT INTO admins (id, first_name, last_name, phone, photo, passport, district) " \
-                f"VALUES('{data['admin_id']}', '{data['admin_first_name']}', '{data['admin_last_name']}', '{data['admin_phone']}', '{data['admin_photo']}', '{data['admin_passport']}', '{data['admin_district']}') "
+        query = f"INSERT INTO admins (id, first_name, last_name, phone, photo, passport) " \
+                f"VALUES('{data['admin_id']}', '{data['admin_first_name']}', '{data['admin_last_name']}', '{data['admin_phone']}', '{data['admin_photo']}', '{data['admin_passport']}') "
+        await self.connector.execute(query)
+
+    async def add_data_volunteer(self, data):
+        query = f"INSERT INTO volunteers (id, forename, surname, email, phone_number, photo_id, passport, food_balance)" \
+                f"VALUES('{data['volunteer_id']}', '{data['volunteer_first_name']}', '{data['volunteer_last_name']}', '{data['volunteer_email']}','{data['volunteer_phone']}', '{data['volunteer_photo_id']}', '{data['volunteer_passport']}', '{data['volunteer_balance']}') "
         await self.connector.execute(query)
 
     async def delete_data_pet(self, petId):
